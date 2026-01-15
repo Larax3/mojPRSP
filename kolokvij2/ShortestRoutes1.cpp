5879ca381f885399e70973b6021e829df9612f5e
Larax3 <164391276+Larax3@users.noreply.github.com>
2026-01-15 12:01:46 +0100

    Create ShortestRoutes1.cpp

---
+++ kolokvij2/ShortestRoutes1.cpp
@@ -0,0 +1,49 @@
+// alias gpp='g++ solution.cpp -o s && ./s'
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long INF = 1e18;
+
+int main(){
+    freopen("ulaz.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<pair<int,long long>>> adj(n+1);
+    for(int i = 0; i < m; i++){
+        int a, b;
+        long long c;
+        cin >> a >> b >> c;
+        adj[a].push_back({b, c});
+    }
+
+    vector<long long> dist(n+1, INF);
+    dist[1] = 0;
+
+    priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
+    pq.push({0, 1});
+
+    while(!pq.empty()){
+        auto [d, u] = pq.top();
+        pq.pop();
+
+        if(d != dist[u]) continue;
+
+        for(auto [v, w] : adj[u]){
+            if(dist[u] + w < dist[v]){
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    for(int i = 1; i <= n; i++){
+        cout << dist[i] << (i==n ? "\n" : " ");
+    }
+
+    return 0;
+}
